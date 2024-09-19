be0a036ed5136e9b9000dc1390ea52e9c2908eec
NoneType4Name <90710372+NoneType4Name@users.noreply.github.com>
2024-09-19 14:50:07 +0300

    test of floating windows realization of overlay.

--- conanfile.py
+++ conanfile.py
@@ -11,13 +11,21 @@ class ImGui(ConanFile):
 
     def requirements(self):
         self.requires("imgui/[>=1.89.4]")
-        self.requires("glfw/[>=3.3.8]")
+        self.requires("glfw/[>=3.4]")
         self.requires("spdlog/[>=1.11.0]")
 
     def generate(self):
         tc = CMakeToolchain(self, "Ninja")
         tc.generate()
-        copy(self, "*glfw*", os.path.join(self.dependencies["imgui"].package_folder,
-                                          "res", "bindings"), os.path.join(self.source_folder, "bindings"))
-        copy(self, "*opengl3*", os.path.join(self.dependencies["imgui"].package_folder,
-                                             "res", "bindings"), os.path.join(self.source_folder, "bindings"))
+        copy(
+            self,
+            "*win32*",
+            os.path.join(self.dependencies["imgui"].package_folder, "res", "bindings"),
+            os.path.join(self.source_folder, "bindings"),
+        )
+        copy(
+            self,
+            "*opengl3*",
+            os.path.join(self.dependencies["imgui"].package_folder, "res", "bindings"),
+            os.path.join(self.source_folder, "bindings"),
+        )
